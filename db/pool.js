639df3aff7d5e543089faf9bf25e128013ce2c77
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-01-18 00:39:32 +0800

    connections string fix

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const {Pool} = require("pg");
 
-const connection = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:5432/${process.env.DATABASE_NAME}?ssl=require`;
+const connection = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:5432/${process.env.DATABASE_NAME}`;
 
 const pool = new Pool({
     connectionString : connection
