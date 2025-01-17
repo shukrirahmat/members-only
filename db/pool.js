b319988e45444e7a46f2e27b7541f05df7152e4b
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-01-18 00:51:38 +0800

    connection

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const {Pool} = require("pg");
 
-const connection = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:5432/${process.env.DATABASE_NAME}`;
+const localConnection = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:5432/${process.env.DATABASE_NAME}`;
+const remoteConnection = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:5432/${process.env.DATABASE_NAME}?sslmode=require`;
 
 const pool = new Pool({
-    connectionString : connection
+    connectionString : process.env.BUILD == "local" ? localConnection : remoteConnection
 })
 
 module.exports = pool;
